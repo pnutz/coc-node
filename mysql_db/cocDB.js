0468248c34e1bb38a858185a6d60bc96e1057505
pnutz <pnutz0@gmail.com>
2014-11-15 01:53:32 -0800

    Modified db name to match heroku db name.

--- mysql_db/cocDB.js
+++ mysql_db/cocDB.js
@@ -1,7 +1,7 @@
-module.exports.db = "CREATE DATABASE IF NOT EXISTS counterApp DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
-module.exports.use = "USE counterApp;";
+module.exports.db = "CREATE DATABASE IF NOT EXISTS heroku_06d5fed0deeebe4 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
+module.exports.use = "USE heroku_06d5fed0deeebe4;";
 
-module.exports.tables = ["CREATE TABLE IF NOT EXISTS counterApp.Category ( " +
+module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.Category ( " +
   "idCategory INT NOT NULL AUTO_INCREMENT, " +
   "categoryCode VARCHAR(2) NOT NULL, " +
   "category VARCHAR(45) NOT NULL, " +
@@ -12,7 +12,7 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS counterApp.Category ( " +
   "UNIQUE INDEX categoryCode_UNIQUE (categoryCode ASC)) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS counterApp.Message ( " +
+"CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.Message ( " +
   "idMessage INT NOT NULL AUTO_INCREMENT, " +
   "idCategory INT NULL, " +
   "messageCode VARCHAR(7) NOT NULL, " +
@@ -24,12 +24,12 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS counterApp.Category ( " +
   "UNIQUE INDEX messageCode_UNIQUE (messageCode ASC), " +
   "CONSTRAINT fkCategoryId " +
     "FOREIGN KEY (idCategory) " +
-    "REFERENCES counterApp.Category (idCategory) " +
+    "REFERENCES heroku_06d5fed0deeebe4.Category (idCategory) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS counterApp.User ( " +
+"CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.User ( " +
   "idUser INT NOT NULL AUTO_INCREMENT, " +
   "idDevice VARCHAR(36) NOT NULL, " +
   "uuid VARCHAR(34) NOT NULL, " +
@@ -39,7 +39,7 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS counterApp.Category ( " +
   "PRIMARY KEY (idUser)) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS counterApp.MessageHistory ( " +
+"CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.MessageHistory ( " +
   "idMessageHistory INT NOT NULL AUTO_INCREMENT, " +
   "idSentUser INT NOT NULL, " +
   "idReceivedUser INT NOT NULL, " +
@@ -57,22 +57,22 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS counterApp.Category ( " +
   "INDEX fkCategoryCode_idx (categoryCode ASC), " +
   "CONSTRAINT fkSentUserId " +
     "FOREIGN KEY (idSentUser) " +
-    "REFERENCES counterApp.User (idUser) " +
+    "REFERENCES heroku_06d5fed0deeebe4.User (idUser) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT fkReceivedUserId " +
     "FOREIGN KEY (idReceivedUser) " +
-    "REFERENCES counterApp.User (idUser) " +
+    "REFERENCES heroku_06d5fed0deeebe4.User (idUser) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT fkMessageCode " +
     "FOREIGN KEY (messageCode) " +
-    "REFERENCES counterApp.Message (messageCode) " +
+    "REFERENCES heroku_06d5fed0deeebe4.Message (messageCode) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT fkCategoryCode " +
     "FOREIGN KEY (categoryCode) " +
-    "REFERENCES counterApp.Category (categoryCode) " +
+    "REFERENCES heroku_06d5fed0deeebe4.Category (categoryCode) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION) " +
 "ENGINE = InnoDB;"];
