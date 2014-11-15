455ed1fad7f9529618d10b980c7597f223fb5dc9
pnutz <pnutz0@gmail.com>
2014-11-15 02:01:51 -0800

    Attempted solution to: Incorrect table definition; there can be only one TIMESTAMP column with CURRENT_TIMESTAMP in DEFAULT or ON UPDATE clause This error message is associated with older versions of mysql (as provided by cleardb heroku).

--- mysql_db/cocDB.js
+++ mysql_db/cocDB.js
@@ -6,7 +6,7 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.Cate
   "categoryCode VARCHAR(2) NOT NULL, " +
   "category VARCHAR(45) NOT NULL, " +
   "type VARCHAR(45) NOT NULL, " +
-  "createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+  "createdAt TIMESTAMP NOT NULL DEFAULT 0, " +
   "updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
   "PRIMARY KEY (idCategory), " +
   "UNIQUE INDEX categoryCode_UNIQUE (categoryCode ASC)) " +
@@ -17,7 +17,7 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.Cate
   "idCategory INT NULL, " +
   "messageCode VARCHAR(7) NOT NULL, " +
   "message VARCHAR(140) NOT NULL, " +
-  "createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+  "createdAt TIMESTAMP NOT NULL DEFAULT 0, " +
   "updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
   "PRIMARY KEY (idMessage), " +
   "INDEX idCategory_idx (idCategory ASC), " +
@@ -34,7 +34,7 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.Cate
   "idDevice VARCHAR(36) NOT NULL, " +
   "uuid VARCHAR(34) NOT NULL, " +
   "username VARCHAR(60) NOT NULL, " +
-  "createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+  "createdAt TIMESTAMP NOT NULL DEFAULT 0, " +
   "updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
   "PRIMARY KEY (idUser)) " +
 "ENGINE = InnoDB;",
@@ -48,7 +48,7 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_06d5fed0deeebe4.Cate
   "latitude DECIMAL(9,6) NOT NULL, " +
   "longitude DECIMAL(9,6) NOT NULL, " +
   "receivedAt DATETIME NOT NULL, " +
-  "createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+  "createdAt TIMESTAMP NOT NULL DEFAULT 0, " +
   "updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
   "PRIMARY KEY (idMessageHistory), " +
   "INDEX fkSentUserId_idx (idSentUser ASC), " +
